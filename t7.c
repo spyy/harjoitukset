c2ee5eca85128aec9b6371298a76d06507a13cc5
spyy <sami.pyy@gmail.com>
2014-09-20 08:19:23 +0300

    Tehtävä 7

---
+++ t7.c
@@ -0,0 +1,23 @@
+// t7.c sami pyy
+
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+	char c;
+	const char *taateli = "taateli";
+	const char *p;
+	
+	printf("Anna merkki\n");
+	scanf("%c", &c);
+	p = strchr(taateli, c);
+	
+	if(p)
+		printf("Löytyi merkki: %c", *p);
+	else
+		printf("Merkkiä ei löytynyt");
+	
+	return 0;
+}
+
